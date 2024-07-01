174ccf4be5a9dfbe132c47e71dd90b6b51792783
rtk5 <rithvik.matta@gmail.com>
2024-07-01 11:39:45 +0530

    jul 1

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int a=0;
+    int b=2;
+    printf("%d\n",a+b);
+    return 0;
+}
+
+/*
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+  */
