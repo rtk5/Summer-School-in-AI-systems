c49c6e1b83929924b4b60fa81adf0384d22b9ae3
rtk5 <rithvik.matta@gmail.com>
2024-07-03 16:39:20 +0530

    working compiler

--- compile.py
+++ compile.py
@@ -5,16 +5,38 @@ inp = json.loads(sys.stdin.read())
 
 def gen_head(header):
     fn_name = header[0]
-    args = header[1:]  
-    out_args = [[a, "int"] for a in args]  
+    args = header[1:]  # Extract arguments from the header
+    out_args = [[a, "int"] for a in args]  # Assume all arguments are of type "int"
     return [fn_name, "int"], out_args
 
 def gen_body(body):
-    return [body[0], body[1]]  
+    assert body[0] == 'ret'
+    expr = body[1]
+    if not isinstance(expr, list):
+        return [body]  # Handle simple return values
+    else:
+        out = []
+        operator = expr[0]
+        op1 = expr[1]
+        op2 = expr[2]
+        out_set = ['set', 'tmp', 'int']
+        if operator == '+':
+            out_set.append(['add', op1, op2])
+        elif operator == '-':
+            out_set.append(['sub', op1, op2])
+        elif operator == '*':
+            out_set.append(['mul', op1, op2])
+        elif operator == '/':
+            out_set.append(['div', op1, op2])
+        else:
+            raise Exception("Error - Invalid operation type")
+        out.append(out_set)
+        out.append(['ret', 'tmp'])
+        return out
 
 def compile(inp):
     assert inp[0] == 's-kaleidoscope'
-
+    
     function_defns = inp[1:]
     bril_defns = []
     
@@ -30,10 +52,11 @@ def compile(inp):
         bril_defns.append(["define", [head[0], *head[1]], body])
     
     return ["brilisp"] + bril_defns
+
 try:
     output = compile(inp)
     print(json.dumps(output))
 except Exception as e:
     print(f"Error: {str(e)}")
-
+    
 #["brilisp", ["define", [["fn", "n"], ["n", "int"]], ["ret", "n"]]]
